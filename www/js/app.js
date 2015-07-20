a04447c1abe093b9375b52341c910fd8221bc942
Isak Rabin <isak_r@yahoo.com>
2015-07-21 01:21:39 +0800

    Add Notification

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,26 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', [
+  'ionic',
+  'ngCordova',
+  'ionic.service.core',
+  'ionic.service.push',
+  'starter.controllers',
+  'starter.services'
+])
+
+.config(['$ionicAppProvider', function($ionicAppProvider) {
+  // Identify app
+  $ionicAppProvider.identify({
+    // The App ID (from apps.ionic.io) for the server
+    app_id: 'd6ebe5d0',
+    // The public API key all services will use for this app
+    api_key: '5c4e72d86030b23ed7f5bd9058764b5ba570667432091e78',
+    // Set the app to use development pushes
+    dev_push: true
+  });
+}])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
